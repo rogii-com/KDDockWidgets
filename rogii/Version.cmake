e03e454c5a0f46a4c0678b78978e589960f48db4
Alexey Kostin <39875329+chak3r@users.noreply.github.com>
2021-03-26 14:15:20 +0400

    Rogii v1.3.0 dev (#1)
    * Added script for build package(not completed)
    
    * moved to rogii instead of cmake
    
    * Added Utils_p.h to istall section. Actualized package.cmake
    
    * Added comments about find_package
    
    * deleted extra package.cmake
    
    * fixed include cnpm
    
    * fixed mistype
    
    * Separated debug and release version on linux
    
    * Fixed debug suffix
    
    * Increased windowsSDK package build number
    
    * Fixed msvs packages
    
    * Fixed generating pdb files for static lib. Fixed path to debug lib under windows in package.cmake
    
    * Added scripts for building on Jenkins
    
    Co-authored-by: Alexey Kostin <a.kostin@rogii.com>

---
+++ rogii/Version.cmake
@@ -0,0 +1,19 @@
+set(
+    KDDockWidgets_VERSION_MAJOR
+    1
+)
+
+set(
+    KDDockWidgets_VERSION_MINOR
+    3
+)
+
+set(
+    KDDockWidgets_VERSION_PATCH
+	0
+)
+
+set(
+    KDDockWidgets_VERSION
+    "${KDDockWidgets_VERSION_MAJOR}.${KDDockWidgets_VERSION_MINOR}.${KDDockWidgets_VERSION_PATCH}"
+)
